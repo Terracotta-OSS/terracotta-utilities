b5eb0946ffc9ad7e034606504532924fe309ceab
Clifford W. Johnson <clifford.johnson@softwareag.com>
2020-04-28 11:30:53 -0400

    Use system-dependent, world-accessible common file location
    This commit updates GlobalFilePortLocker to create/reuse the
    port lock file in a system-dependent, generally accessible
    common file location (as supported by
    org.terracotta.utilities.test.io.CommonFiles.createCommonAppFile)
    instead of relying on `java.io.tmpdir` -- which can be
    user-specific.  On *NIX systems, the location used is `/var/tmp`
    (in compliance with the Filesystem Hierarchy Standard).  On
    Windows systems, this is the directory assigned to the
    `CommonApplicationData` special folder.

--- port-chooser/src/main/java/org/terracotta/utilities/test/port_locking/GlobalFilePortLocker.java
+++ port-chooser/src/main/java/org/terracotta/utilities/test/port_locking/GlobalFilePortLocker.java
@@ -17,44 +17,48 @@ package org.terracotta.utilities.test.port_locking;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.terracotta.utilities.test.io.CommonFiles;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
-import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
-import java.util.Set;
-
-import static java.nio.file.attribute.PosixFilePermissions.asFileAttribute;
-import static java.nio.file.attribute.PosixFilePermissions.fromString;
 
 public class GlobalFilePortLocker implements PortLocker {
   private static final Logger LOGGER = LoggerFactory.getLogger(GlobalFilePortLocker.class);
-  private static final String TEMP_DIRECTORY_PROPERTY = "java.io.tmpdir";
-  private static final String PORT_LOCK_FILENAME = "tc-port-lock";
-  private static final Set<PosixFilePermission> PERMISSIONS = fromString("rw-rw-rw-");
 
   private final File portLockFile;
 
   public GlobalFilePortLocker() {
-    String tempDirectory = System.getProperty(TEMP_DIRECTORY_PROPERTY);
 
-    if (tempDirectory == null) {
-      throw new PortLockingException("Unable to find the OS temp directory. This is platform dependent, but can be affected by system properties.");
-    }
-
-    Path path = Paths.get(tempDirectory);
-    Path portLockFilePath = path.resolve(PORT_LOCK_FILENAME);
-
+    Path portLockRelative = Paths.get("terracotta", "tc-port-lock");
     try {
-      portLockFile = createOrReuse(portLockFilePath);
+      /*
+       * Create or reuse the lock file in a system-dependent, world-writable location.
+       * The permissions on the file are updated, if possible, at creation time
+       * and when reused so all users have the same permissions as the creator/owner.
+       */
+      Path portLockFilePath = CommonFiles.createCommonAppFile(portLockRelative);
+      portLockFile = portLockFilePath.toFile();
+
+      /*
+       * If the file returned is an existing file, it _should_ be readable and writable
+       * but we need to check ...
+       */
+      if (!Files.isReadable(portLockFilePath)) {
+        throw new PortLockingException("File is not readable: " + portLockFilePath);
+      }
+      if (!Files.isWritable(portLockFilePath)) {
+        throw new PortLockingException("File is not writable: " + portLockFilePath);
+      }
+
+      LOGGER.info("Using port lock file: {}", portLockFilePath);
     } catch (IOException e) {
-      throw new PortLockingException("Failed to create port lock file: " + portLockFilePath, e);
+      throw new PortLockingException("Failed to create " + portLockRelative + " in system common directory", e);
     }
   }
 
@@ -78,28 +82,5 @@ public class GlobalFilePortLocker implements PortLocker {
     } catch (IOException e) {
       throw new PortLockingException("Error while trying to lock port", e);
     }
-  }
-
-  private static File createOrReuse(Path portLockFilePath) throws IOException {
-    File file;
-    try {
-      try {
-        file = Files.createFile(portLockFilePath, asFileAttribute(PERMISSIONS)).toFile();
-      } catch (UnsupportedOperationException e) {
-        // in case we cannot atomically create and set these permissions / rwx not supported we retry to create normally
-        file = Files.createFile(portLockFilePath).toFile();
-      }
-      LOGGER.info("Created port lock file: {}", file.getAbsolutePath());
-    } catch (FileAlreadyExistsException e) {
-      if (!Files.isReadable(portLockFilePath)) {
-        throw new PortLockingException("File is not readable: " + portLockFilePath, e);
-      }
-      if (!Files.isWritable(portLockFilePath)) {
-        throw new PortLockingException("File is not writable: " + portLockFilePath, e);
-      }
-      file = portLockFilePath.toFile();
-      LOGGER.info("Using existing port lock file: {}", file.getAbsolutePath());
-    }
-    return file;
   }
 }
