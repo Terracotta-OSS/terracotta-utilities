2fc320e35488b40ca52cfd5b787d3c88d67958a4
Clifford W. Johnson <clifford.johnson@softwareag.com>
2023-11-17 18:21:16 -0500

    Set user.name in addition to user.home

--- .github/workflows/propagateUserHome.gradle.kts
+++ .github/workflows/propagateUserHome.gradle.kts
@@ -4,6 +4,7 @@
  */
 val userHome: String? = System.getProperty("user.home")
 if (userHome.isNullOrEmpty() || userHome.equals("?")) {
+    val userName = System.getProperty("user.name", "ci")
     val home = System.getenv("HOME")
     if (home.isNullOrEmpty()) {
         throw IllegalStateException("environment variable \"HOME\" must be set when user.home='${userHome}'")
@@ -14,15 +15,18 @@ if (userHome.isNullOrEmpty() || userHome.equals("?")) {
      */
     logger.quiet("Setting user.home=\"${home}\"")
     System.setProperty("user.home", home)
+    System.setProperty("user.name", userName)
 
     gradle.allprojects {
         tasks.withType<Test> {
             logger.quiet("Setting ${this.path} user.home=\"${home}\"")
             systemProperty("user.home", home)
+            systemProperty("user.name", userName)
         }
         tasks.withType<JavaExec> {
             logger.quiet("Setting ${this.path} user.home=\"${home}\"")
             systemProperty("user.home", home)
+            systemProperty("user.name", userName)
         }
     }
 } else {
